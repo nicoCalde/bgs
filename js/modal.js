da42501d5cf0a8ff76067b7e88e200b65a7e6504
nicoCalde <nicogonzalezcalderon@gmail.com>
2022-06-09 22:33:53 -0300

    js modal validation

--- js/modal.js
+++ js/modal.js
@@ -19,3 +19,62 @@ window.addEventListener('click', (i) => {
         modal.style.display = 'none';
     }
 });
+
+// form validation
+const form = document.getElementById('form');
+const name = document.getElementById('modalName');
+const email = document.getElementById('modalEmail');
+const phone = document.getElementById('modalPhone');
+const subject = document.getElementById('modalSubject');
+
+// show error message
+function showError(input, message) {
+    const formValidation = input.parentElement;
+    formValidation.className = 'formValidation error';
+
+    const errorMessage = formValidation.querySelector('p');
+    errorMessage.innerText = message;
+}
+
+// show valid message
+function showValid(input) {
+    const formValidation = input.parentElement;
+    formValidation.className = 'formValidation valid';
+}
+
+// check required fields
+function checkRequired(inputArr) {
+    inputArr.forEach(function(input) {
+        if(input.value.trim() === '') {
+            showError(input, `Se requiere un ${getFieldName(input)}`);
+        } else{
+            showValid(input);
+        }
+    });
+};
+
+// check input length
+function checkLength(input, min, max) {
+    if(input.value.length < min) {
+        showError(input, `El ${getFieldName(input)} tiene que tener al menos ${min} caráteres`);
+    } else if(input.value.length > max) {
+        showError(input, `El ${getFieldName(input)} tiene que ser menor a ${max} caráteres`);
+    } else {
+        showValid(input);
+    }
+}
+
+// get fieldname
+function getFieldName(input) {
+    return input.name.charAt(0).toUpperCase() + input.name.slice(1);
+}
+
+// event listeners
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    checkRequired([name, email, phone, subject]);
+    checkLength(name, 3, 30);
+    checkLength(phone, 8, 18);
+
+});
